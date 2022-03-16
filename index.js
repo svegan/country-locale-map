c1d4c848bfea5931a2f413a26b3bd68bad2c77e5
Grigory Manusha <dev@griman.io>
2022-03-16 13:51:24 +0300

    Fix issue with undefined process variable

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const fuzz = require('fuzzball');
 function CLM() {
     var clm = {};
-    var countries; 
-
-    if(process && process.env && process.env.CLM_MODE == 'INTL') {
-        countries = require('./countries-intl.json');
-    } else {
-        countries = require('./countries.json');
-    }
-
+    var countries = require('./countries.json');
     var countryByAlpha2Code = {};
     var countryByAlpha3Code = {};
     var countryByNumericCode = {};
